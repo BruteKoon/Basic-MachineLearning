570ab6051f066ced69b8cbabbc08608889aa30ef
BruteKoon <BruteKoon@gmail.com>
2018-01-07 10:49:44 -0800

    gradient explanation

--- 7_gradient_descent.py
+++ 7_gradient_descent.py
@@ -6,16 +6,19 @@ W = tf.Variable(tf.random_normal([1]), name = 'weight')
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
+# hypothesis for linear model X * W
 hypothesis  = X * W
 
+# cost function
 cost = tf.reduce_sum(tf.square(hypothesis - Y))
 
-
+#Minimize : gradient DEscent using derivative: W -= Learning_rate * derivative
 learning_rate = 0.1
 gradient =tf.reduce_mean((W * X - Y) * X)
 descent = W - learning_rate * gradient
 update = W.assign(descent)
 
+#Launch the graph in a session
 sess = tf.Session()
 
 sess.run(tf.global_variables_initializer())
