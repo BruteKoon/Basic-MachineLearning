bc3ec5a52964b1dbb258d9b8e62d586e9c86043e
BruteKoon <BruteKoon@gmail.com>
2018-01-23 14:22:22 -0800

    generalization

--- project04_generalization_about_second.py
+++ project04_generalization_about_second.py
@@ -4,24 +4,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.cm as cm
 import random
+import tensorflow as tf
 
 # "txt" -> array
 text_array = np.loadtxt('txt/purdue_P2_unloaded_up_down.txt')
 text_array = np.transpose(text_array)
 fs = 44100
 
+# specify length of time slice in sec
+time_slice = 5
+data_per_time_slice = int(fs*time_slice)
+
 # for loop
-for i in range(1):
+for i in range(3):
 	#select random number
-	rand_num = random.randrange(0,441000)
+	rand_num = random.randrange(0,text_array[1].shape[0] - data_per_time_slice)
 	#make a random_array
-	random_array = text_array[1][rand_num:rand_num+441000]
+	random_array = text_array[2][rand_num:rand_num+data_per_time_slice]
 	
 	#view graph
 	f, t, S = signal.stft(random_array, fs, window = 'hamm', nperseg=256, nfft = 1024)
 	S = np.abs(S)
 	S = 20*np.log10(S + 1e-6)
-
+	
+	#normalization
 	maximum = max(max(S.reshape([1, -1])))
 	minimum = min(min(S.reshape([1, -1])))
 	S_norm = (S - minimum)/(maximum-minimum)
@@ -30,14 +36,25 @@ for i in range(1):
 	plt.title('STFT Magnitude')
 	plt.ylabel('Frequency [Hz]')
 	plt.xlabel('Time [sec]')
-	plt.show()
+#	plt.show()
 	
 	
-	#picture's array 
-	print (S_norm)
-	print(S_norm.shape) #(513, 3447)
+#	picture's array 
+#	print (S_norm)
+#	print(S_norm.shape) #(513,1724)
+
+	#make data_set (1 array = 884412)
+	if i is 0:
+		data_set = S_norm.reshape(-1)
+	else:
+		data_set = np.vstack((data_set, S_norm.reshape(-1)))
 
 
+print (data_set)
+print (data_set.shape)
+
+####################### CNN ##################################
+########### INPUT_DATA = S_norm(2demension_array) #############################
 
 
 
